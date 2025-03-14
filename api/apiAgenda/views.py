1b3558f9e977e36d46c2f43b0e8aedc6ae036dba
Henri Okayama <henri.okayama@gmail.com>
2025-03-13 22:21:36 -0300

    precisa terminar o crud

--- api/apiAgenda/views.py
+++ api/apiAgenda/views.py
@@ -1,7 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 from rest_framework import status
 from .models import Contato
 from .serializers import ContatoSerializer
+
 
 #Criar Endpoints
 @api_view(['POST'])
@@ -11,3 +14,16 @@ def inserirContato(request):
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
     return Response(serializer.erros, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET'])
+def listarContato(request):
+    contatos = Contato.objects.all()
+    serializer = ContatoSerializer(contatos, many=True)
+    return Response(serializer.data)
+
+@api_view(['PUT'])
+def atualizarContato(request, pk):
+    try:
+        contato = Contato.objects.get(id = pk)
+    except Contato.DoesNotExist
+        return Response(status=status.HTTP_404_NOT_FOUND)
